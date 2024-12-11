8d25a6afd43b85562b0e30b0c072918103e35abb
gnmathew <genemathewdorado@gmail.com>
2024-12-11 15:37:15 +0800

    [Feature #2] Create User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2024_12_11_073205) do
+  create_table "users", charset: "utf8mb4", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
